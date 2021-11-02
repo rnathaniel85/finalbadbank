883af0401af566889e7e242d56053c59e6d7b5a9
Richard Nathaniel <78697102+rnathaniel85@users.noreply.github.com>
2021-11-02 07:30:03 -0400

    Add files via upload

--- dal.js
+++ dal.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const url         = 'mongodb://localhost:27017';
+const url         = 'mongodb+srv://rnathaniel85:<password>@serverlessinstance0.oyxlg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 let db            = null;
  
 // connect to mongo
