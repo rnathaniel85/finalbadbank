0d36b98796925c5b1f8f62a0f80dd476064db54c
Richard Nathaniel <78697102+rnathaniel85@users.noreply.github.com>
2021-11-02 07:26:59 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -150,6 +150,6 @@ app.get('/account/all', function (req, res) {
     });
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 app.listen(port);
 console.log('Running on port: ' + port);
